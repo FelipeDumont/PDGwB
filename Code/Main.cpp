282d051ee37ad17b59b6dc04b7688d2646eec2fd
FelipeDumont <felipe.dumont.ortiz@gmail.com>
2023-08-16 13:21:23 -0400

    Update bash
    Update bash to be able to make file without moving files

--- Code/Main.cpp
+++ Code/Main.cpp
@@ -4,6 +4,5 @@
 
 int main() {
     std::cout << "Hello There stranger" << std::endl;
-
     return 0;
 }
--- Code/test.sh
+++ Code/test.sh
@@ -7,16 +7,18 @@ clear
 make clean
 make
 
-# Test and play
-echo "------------------------------------------------------------------"
-echo "./PDGWB"
-./PDGwB
-# Update the files in the Local Git Files
-# Remove Old Files
-rm ../sf/PDGwB/*
-# Replace them ???
-files=`ls`
-for eachFile in $files
-do
- cp $eachFile ../sf/PDGwB/Code
-done
+if(($1 ==0));then
+ # Test and play
+ echo "------------------------------------------------------------------"
+ echo "./PDGWB"
+ ./PDGwB
+ # Update the files in the Local Git Files
+ # Remove Old Files
+ rm ../sf/PDGwB/*
+ # Replace them ???
+ files=`ls`
+ for eachFile in $files
+ do
+  cp $eachFile ../sf/PDGwB/Code
+ done
+fi
